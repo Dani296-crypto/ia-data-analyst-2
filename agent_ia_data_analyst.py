7580b3a64a798cd145c36255a2145222ef204535
Dani296-crypto <abalod296@gmail.com>
2026-05-19 11:21:18 +0000

    Update agent_ia_data_analyst.py

--- agent_ia_data_analyst.py
+++ agent_ia_data_analyst.py
@@ -9,8 +9,9 @@ password = st.text_input("Mot de passe", type="password")
 if st.button("Se connecter"):
 
     if password == st.secrets["APP_PASSWORD"]:
+        st.session_state["auth"] = True
         st.success("Accès autorisé")
-        st.switch_page("pages/dashboard.py")
+        st.rerun()
 
     else:
         st.error("Mot de passe incorrect")
